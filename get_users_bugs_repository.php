9f7ac998e15ff15aabd9514e51b7dcb89f2504ed
janirapo <rapo.jani@gmail.com>
2017-01-25 13:08:44 +0200

    Create get_users_bugs_repository.php

---
+++ get_users_bugs_repository.php
@@ -0,0 +1,17 @@
+<?php
+// get_users_bugs_repository.php
+require_once "bootstrap.php";
+
+$userId = $argv[1];
+
+$bugs = $entityManager->getRepository('Bug')->getUsersBugs($userId);
+
+foreach ($bugs as $bug) {
+    echo $bug->getDescription()." - ".$bug->getCreated()->format('d.m.Y')."\n";
+    echo "    Reported by: ".$bug->getReporter()->getName()."\n";
+    echo "    Assigned to: ".$bug->getEngineer()->getName()."\n";
+    foreach ($bug->getProducts() as $product) {
+        echo "    Platform: ".$product->getName()."\n";
+    }
+    echo "\n";
+}
